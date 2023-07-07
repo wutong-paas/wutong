2251fc343c0ef5429abaff97a203b3356c869282
poneding <poneding@gmail.com>
2023-07-07 17:07:40 +0800

    init v1.4.0 version

--- arm64-release.sh
+++ arm64-release.sh
@@ -2,7 +2,7 @@
 # !!! You should run this script on linux/arm64 arch.
 
 export BUILD_ARCH=arm64
-export VERSION=v1.3.0-arm64
+export VERSION=v1.4.0-arm64
 ./release.sh all push
 
 # build latest mesh-data-panel and init-probe
